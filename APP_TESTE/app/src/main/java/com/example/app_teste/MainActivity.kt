33ba611b9b47ebbcd1d56643a0c94672dd7a088d
LucasDonizete08 <lucas.dsantos@bandtec.com.br>
2022-05-25 15:11:33 -0300

    update

--- APP_TESTE/app/src/main/java/com/example/app_teste/MainActivity.kt
+++ APP_TESTE/app/src/main/java/com/example/app_teste/MainActivity.kt
@@ -11,101 +11,106 @@ import androidx.core.view.*
 class MainActivity : AppCompatActivity() {
     lateinit var resposta_1: LinearLayout;
     lateinit var resposta_2: LinearLayout;
+    lateinit var resposta_3: LinearLayout;
     lateinit var pergunta_1: LinearLayout;
     lateinit var pergunta_2: LinearLayout;
     lateinit var pergunta_3: LinearLayout;
-    lateinit var img_pergunta_1: ImageView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         resposta_1 = findViewById(R.id.resposta_1);
         resposta_2 = findViewById(R.id.resposta_2);
+        resposta_3 = findViewById(R.id.resposta_3);
         pergunta_1 = findViewById(R.id.pergunta_1);
         pergunta_2 = findViewById(R.id.pergunta_2);
         pergunta_3= findViewById(R.id.pergunta_3);
     }
 
     fun teste1(V: View) {
-        val sideBarParam = resposta_1.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam2 = pergunta_2.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam3 = pergunta_3.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam4 = resposta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta1Param = resposta_1.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta2Param = pergunta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta2Param = resposta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta3Param = pergunta_3.layoutParams as ViewGroup.MarginLayoutParams
+        val resposta3Param = resposta_3.layoutParams as ViewGroup.MarginLayoutParams
 
         var top = pergunta_1.marginTop
         var bottom = pergunta_1.marginBottom
         var right = pergunta_1.marginRight
         var left = pergunta_1.marginLeft
         val top_2 = pergunta_2.marginTop
-        val bottom_2 = pergunta_2.marginBottom
-        val right_2 = pergunta_2.marginRight
-        val left_2 = pergunta_2.marginLeft
         val top_3 = pergunta_3.marginTop
-        val bottom_3 = pergunta_3.marginBottom
-        val right_3 = pergunta_3.marginRight
-        val left_3 = pergunta_3.marginLeft
-        val top_4 = resposta_2.marginTop
-        val bottom_4 = resposta_2.marginBottom
-        val right_4 = resposta_2.marginRight
-        val left_4 = resposta_2.marginLeft
-        if (sideBarParam.topMargin == 0 && sideBarParam4.topMargin < 500) {
-            sideBarParam.setMargins(left, top+195, right, bottom);
-            sideBarParam2.setMargins(left_2, top_2+195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3+195, right_3, bottom_3);
+        if (resposta_1.isVisible == false && resposta_2.isVisible == false&& resposta_3.isVisible == false) {
+            resposta1Param.setMargins(left, top+195, right, bottom);
+            pergunta2Param.setMargins(left, top_2+195, right, bottom);
+            pergunta3Param.setMargins(left, top_3+195, right, bottom);
 
-            resposta_1.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
-            resposta_2.layoutParams = sideBarParam4
-            pergunta_1.background
-            resposta_1.isVisible = true
-
-        }
-        else if(sideBarParam.topMargin== 0 && sideBarParam4.topMargin > 400){
-            sideBarParam.setMargins(left, top+195, right, bottom);
-            sideBarParam2.setMargins(left_2, top_2+195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3+195, right_3, bottom_3);
-            sideBarParam4.setMargins(left_2, top_2+390, right_2, bottom_2);
-
-            resposta_1.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
-            resposta_2.layoutParams = sideBarParam4
+            resposta_1.layoutParams = resposta1Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
             resposta_1.isVisible = true
         }
-        else if(resposta_2.isVisible == true){
-            sideBarParam.setMargins(0, 0, 0,0);
-            sideBarParam2.setMargins(left_2, top_2-195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3-195, right_3, bottom_3);
-            sideBarParam4.setMargins(left_3, top_2, right_3, bottom_3);
-            resposta_1.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
-            resposta_2.layoutParams = sideBarParam4
+        else if(resposta_1.isVisible == false && resposta_2.isVisible == true&& resposta_3.isVisible == false){
+            resposta1Param.setMargins(left, top+195, right, bottom);
+            pergunta2Param.setMargins(left, top_2+195, right, bottom);
+            resposta2Param.setMargins(left, top_2+390, right, bottom);
+            pergunta3Param.setMargins(left, top_3+195, right, bottom);
 
-            resposta_1.isVisible = false
+            resposta_1.layoutParams = resposta1Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
+            resposta_2.layoutParams = resposta2Param
+            resposta_1.isVisible = true
         }
+        else if(resposta_1.isVisible == false && resposta_2.isVisible == false&& resposta_3.isVisible == true){
+            resposta1Param.setMargins(left, top+195, right, bottom);
+            pergunta2Param.setMargins(left, top_2+195, right, bottom);
+            pergunta3Param.setMargins(left, top_3+195, right, bottom);
+            resposta3Param.setMargins(left, top_3+390, right, bottom);
 
+            resposta_1.layoutParams = resposta1Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
+            resposta_3.layoutParams = resposta3Param
+            resposta_1.isVisible = true
+        }
+        else if(resposta_1.isVisible == false && resposta_2.isVisible == true && resposta_3.isVisible == true){
+            resposta1Param.setMargins(left, top+195, right, bottom);
+            pergunta2Param.setMargins(left, top_2+195, right, bottom);
+            resposta2Param.setMargins(left, top_2+390, right, bottom);
+            pergunta3Param.setMargins(left, top_3+195, right, bottom);
+            resposta3Param.setMargins(left, top_3+390, right, bottom);
+
+            resposta_1.layoutParams = resposta1Param
+            pergunta_2.layoutParams = pergunta2Param
+            resposta_2.layoutParams = resposta2Param
+            pergunta_3.layoutParams = pergunta3Param
+            resposta_3.layoutParams = resposta3Param
+            resposta_1.isVisible = true
+        }
         else {
 
-            sideBarParam.setMargins(0, 0, 0,0);
-            sideBarParam2.setMargins(left_2, top_2-195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3-195, right_3, bottom_3);
-            resposta_1.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
-            resposta_2.layoutParams = sideBarParam4
+            resposta1Param.setMargins(0, 0, 0,0);
+            pergunta2Param.setMargins(left, top_2-195, right, bottom);
+            resposta2Param.setMargins(left, top_2, right, bottom);
+            pergunta3Param.setMargins(left, top_3-195, right, bottom);
+            resposta3Param.setMargins(left, top_3, right, bottom);
+            resposta_1.layoutParams = resposta1Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
+            resposta_2.layoutParams = resposta2Param
+            resposta_3.layoutParams = resposta3Param
 
             resposta_1.isVisible = false
 
         }
     }
     fun teste2(V: View) {
-        val sideBarParam4 = resposta_1.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam = resposta_2.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam2 = pergunta_2.layoutParams as ViewGroup.MarginLayoutParams;
-        val sideBarParam3 = pergunta_3.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta1Param = resposta_1.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta2Param = resposta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta3Param = resposta_3.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta2Param = pergunta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta3Param = pergunta_3.layoutParams as ViewGroup.MarginLayoutParams;
 
 
         val top_2 = pergunta_2.marginTop
@@ -117,40 +122,65 @@ class MainActivity : AppCompatActivity() {
         val right_3 = pergunta_3.marginRight
         val left_3 = pergunta_3.marginLeft
 
-        if (sideBarParam.topMargin == 0) {
-            sideBarParam.setMargins(left_2, top_2+195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3+195, right_3, bottom_3);
-            resposta_2.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
+        if (resposta_2.isVisible == false && resposta_3.isVisible == false) {
+            resposta2Param.setMargins(left_2, top_2+195, right_2, bottom_2);
+            pergunta3Param.setMargins(left_3, top_3+195, right_3, bottom_3);
+            resposta_2.layoutParams = resposta2Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
 
             resposta_2.isVisible = true
 
-        }else if (sideBarParam.topMargin == 0 && sideBarParam4.topMargin >=345 && sideBarParam3.topMargin <= 790) {
-            sideBarParam.setMargins(left_2, top_2+195, right_2, bottom_2);
-            sideBarParam3.setMargins(left_3, top_3+195, right_3, bottom_3);
-            resposta_2.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
+        }else if (resposta_2.isVisible == false && resposta_3.isVisible == true) {
+            resposta2Param.setMargins(left_2, top_2+195, right_2, bottom_2);
+            pergunta3Param.setMargins(left_3, top_3+195, right_3, bottom_3);
+            resposta3Param.setMargins(left_3, top_3+390, right_3, bottom_3);
+            resposta_2.layoutParams = resposta2Param
+            resposta_3.layoutParams = resposta3Param
+            pergunta_2.layoutParams = pergunta2Param
+            pergunta_3.layoutParams = pergunta3Param
 
             resposta_2.isVisible = true
 
-        }else if (sideBarParam.topMargin == 0 && sideBarParam4.topMargin >=345 && sideBarParam3.topMargin > 790) {
-            sideBarParam.setMargins(left_2, top_2+195, right_2, bottom_2);
-            resposta_2.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
-
-            resposta_2.isVisible = true
-
-        } else {
-            sideBarParam.setMargins(0, 0, 0,0);
-            sideBarParam3.setMargins(left_3, top_3-195, right_3, bottom_3);
-            resposta_2.layoutParams = sideBarParam
-            pergunta_2.layoutParams = sideBarParam2
-            pergunta_3.layoutParams = sideBarParam3
+        }else {
+            resposta2Param.setMargins(0, 0, 0,0);
+            pergunta3Param.setMargins(left_3, top_3-195, right_3, bottom_3);
+            resposta3Param.setMargins(left_3, top_3, right_3, bottom_3);
+            resposta_2.layoutParams = resposta2Param
+            pergunta_3.layoutParams = pergunta3Param
+            resposta_3.layoutParams = resposta3Param
 
             resposta_2.isVisible = false
+
+        }
+    }
+    fun teste3(V: View) {
+        val resposta2Param = resposta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val resposta3Param = resposta_3.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta2Param = pergunta_2.layoutParams as ViewGroup.MarginLayoutParams;
+        val pergunta3Param = pergunta_3.layoutParams as ViewGroup.MarginLayoutParams;
+
+
+
+        val top_2 = pergunta_2.marginTop
+        val bottom_2 = pergunta_2.marginBottom
+        val right_2 = pergunta_2.marginRight
+        val left_2 = pergunta_2.marginLeft
+        val top_3 = pergunta_3.marginTop
+        val bottom_3 = pergunta_3.marginBottom
+        val right_3 = pergunta_3.marginRight
+        val left_3 = pergunta_3.marginLeft
+
+        if (resposta_3.isVisible == false) {
+            resposta3Param.setMargins(left_3, top_3+195, right_3, bottom_3);
+            resposta_3.layoutParams = resposta3Param
+            resposta_3.isVisible = true
+
+        }else {
+            resposta3Param.setMargins(0, 0, 0,0);
+            resposta_3.layoutParams = resposta3Param
+
+            resposta_3.isVisible = false
 
         }
     }
